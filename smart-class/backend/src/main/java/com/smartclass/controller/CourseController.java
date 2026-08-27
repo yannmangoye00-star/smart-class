dd5890de356720805bc0a98a496daa6f55c665f8
Yann Mangoye <yannmangoye00@gmail.com>
2026-08-27 17:10:50 +0200

    Fix imports dans CourseController et mise a jour CORS

--- smart-class/backend/src/main/java/com/smartclass/controller/CourseController.java
+++ smart-class/backend/src/main/java/com/smartclass/controller/CourseController.java
@@ -1,7 +1,19 @@
+package com.smartclass.controller;
+
+import com.smartclass.dto.CourseDTO;
+import com.smartclass.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
-@CrossOrigin(origins = "http://localhost:5173") // URL de Vite
+@CrossOrigin(origins = "*") // Permet les requêtes depuis Vercel ou localhost
 public class CourseController {
 
     @Autowired
